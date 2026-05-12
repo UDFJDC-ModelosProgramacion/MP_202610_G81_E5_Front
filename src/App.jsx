05cc003dd1682de3e9ed131327f9bc07547cdc98
SantiagoRincon43 <josrincona@udistrital.edu.co>
2026-05-12 18:31:39 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom'; 
-
 import RegistrarEventoPage from './modules/lifeevents/RegistrarEventoPage';
 import SolicitarAdopcionPage from './modules/adoptions/SolicitarAdopcionPage';
 import CrearEventoRefugioPage from './modules/shelter/CrearEventoRefugioPage';
 import RegistrarConvivenciaPage from './modules/cohabitation/RegistrarConvivenciaPage';
+import RegisterForm from './modules/auth/RegisterForm';
+import RegistrarMascotaPage from './modules/pets/RegistrarMascotaPage';
+import ConsultarMascotaPage from './modules/pets/ConsultarMascotaPage';
 
 function App() {
   return (
@@ -16,13 +18,18 @@ function App() {
           <p><a href="/solicitar-adopcion">HU08: Solicitar Adopción</a></p>
           <p><a href="/crear-evento-refugio">HU09: Evento de Refugio</a></p>
           <p><a href="/registrar-convivencia">HU10: Convivencia de Prueba</a></p>
+          <p><a href="/crear-cuenta">HU01: Crear Cuenta</a></p>
+          <p><a href="/registrar-mascota">HU02: Registrar Mascota</a></p>
+          <p><a href="/consultar-mascota">HU03: Consultar Mascota</a></p>
         </div>
       } />
-
       <Route path="/registrar-evento-vida" element={<RegistrarEventoPage />} />
       <Route path="/solicitar-adopcion" element={<SolicitarAdopcionPage />} />
       <Route path="/crear-evento-refugio" element={<CrearEventoRefugioPage />} />
       <Route path="/registrar-convivencia" element={<RegistrarConvivenciaPage />} />
+      <Route path="/crear-cuenta" element={<RegisterForm />} />
+      <Route path="/registrar-mascota" element={<RegistrarMascotaPage />} />
+      <Route path="/consultar-mascota" element={<ConsultarMascotaPage />} />
     </Routes>
   );
 }
