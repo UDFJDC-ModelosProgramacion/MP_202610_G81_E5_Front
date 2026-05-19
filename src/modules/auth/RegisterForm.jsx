fea93eb9fca5bb7e4321367244f9ffe6f11539fd
DylanSilva-dot <ddsilvao@udistrital.edu.co>
2026-05-18 19:17:47 -0500

    Cohabitation updater fue agregado

--- src/modules/auth/RegisterForm.jsx
+++ src/modules/auth/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+  import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { registerUser } from '../../services/UserService';
 import './RegisterForm.css';
